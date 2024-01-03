2710d0ef847dad01ec5fc459dde07ec1b9d62e92
Omar Assouma <assouma@sabanciuniv.edu>
2024-01-03 14:43:16 +0200

    removed product-card prop

--- src/components/products-card.tsx
+++ src/components/products-card.tsx
@@ -19,35 +19,28 @@ type ProductsCardProps = {
   noTitle?: boolean;
 };
 
-export const ProductsCard: React.FC<ProductsCardProps> = ({
-  children,
-  noTitle = true,
-}) => {
+export const ProductsCard: React.FC<ProductsCardProps> = ({ children }) => {
   return (
     <div className="flex flex-col h-screen w-screen overflow-hidden">
       <div className="flex-grow overflow-auto">
         <Card className="w-11/12 mx-auto my-9 min-h-[92%] flex flex-col">
           <CardHeader>
-            {!noTitle && (
-              <>
-                <CardTitle>Products</CardTitle>
-                <CardDescription className="flex justify-between">
-                  <Input type="search" placeholder="Search" className="w-1/3" />
-                  <div className="flex md:gap-4 ">
-                    <Dialog
-                      buttonText="Add"
-                      title="Add Product"
-                      okButton="Add"
-                    ></Dialog>
-                    <Dialog
-                      buttonText="Compare"
-                      title="Compare Products"
-                      okButton="Compare"
-                    ></Dialog>
-                  </div>
-                </CardDescription>
-              </>
-            )}
+            <CardTitle>Products</CardTitle>
+            <CardDescription className="flex justify-between">
+              <Input type="search" placeholder="Search" className="w-1/3" />
+              <div className="flex md:gap-4 ">
+                <Dialog
+                  buttonText="Add"
+                  title="Add Product"
+                  okButton="Add"
+                ></Dialog>
+                <Dialog
+                  buttonText="Compare"
+                  title="Compare Products"
+                  okButton="Compare"
+                ></Dialog>
+              </div>
+            </CardDescription>
           </CardHeader>
           <CardContent className="overflow-auto">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
